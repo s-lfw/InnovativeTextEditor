e755998a9b696fe7113c595ee57a22355a74f39d
V.Kosulnikov <v.kosulnikov@gmail.com>
2015-02-08 17:26:12 +0500

    Version 1.0

--- src/test/java/editor/TestLaunch.java
+++ src/test/java/editor/TestLaunch.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class TestLaunch {
     private static final String DICTIONARY_FILE_NAME = "test.in";
     private static final String RESULT_FILE_NAME = "test.out";
-    private static final String JAR_NAME = "InnovativeTextEditor-1.0-SNAPSHOT.jar";
+    private static final String JAR_NAME = "InnovativeTextEditor-1.0.jar";
     public static void main(String[] args) {
         try {
             // reading input data from file
